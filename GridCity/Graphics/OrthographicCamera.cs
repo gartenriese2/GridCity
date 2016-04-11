1b05f635dddb367b1e8a91ac587ab72d171a7cdb
gartenriese2 <bastian.mattes@informatik.stud.uni-erlangen.de>
2016-04-11 19:22:28 +0200

    v0.0.7 - added Width and Height Properties to OrthographicCamera

--- GridCity/Graphics/OrthographicCamera.cs
+++ GridCity/Graphics/OrthographicCamera.cs
@@ -5,8 +5,17 @@
     internal class OrthographicCamera : Camera {
 
         public OrthographicCamera(Vector3 pos, float width, float height, float near, float far) : base(pos) {
+            Width = width;
+            Height = height;
             ProjMat = Matrix.CreateOrthographic(width, height, near, far);
         }
+
+        //---------------------------------------------------------------------
+        // Properties
+        //---------------------------------------------------------------------
+        public float Width { get; }
+
+        public float Height { get; }
 
         // TODO: zoom methods
     }
